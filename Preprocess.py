5e356875e8a6fb8339acb289f095c669c0c6a702
Saket Mule <mulaysaket9@gmail.com>
2020-01-24 19:28:42 +0530

    Add files via upload

---
+++ Preprocess.py
@@ -0,0 +1,79 @@
+# Preprocess.py
+
+import cv2
+import numpy as np
+
+
+
+# module level variables ##########################################################################
+GAUSSIAN_SMOOTH_FILTER_SIZE = (5, 5)
+ADAPTIVE_THRESH_BLOCK_SIZE = 19
+ADAPTIVE_THRESH_WEIGHT = 9
+
+###################################################################################################
+def preprocess(imgOriginal):
+    imgGrayscale = extractValue(imgOriginal)
+
+    imgMaxContrastGrayscale = maximizeContrast(imgGrayscale)
+    
+    height, width = imgGrayscale.shape
+
+    imgBlurred = np.zeros((height, width, 1), np.uint8)
+#    print("IN Preprocess")#me
+#    print("Printing ImgB",imgBlurred)#me
+    imgBlurred = cv2.GaussianBlur(imgMaxContrastGrayscale, GAUSSIAN_SMOOTH_FILTER_SIZE, 0)
+#    print("IN Preprocess")#me
+#    print("Printing ImgBlurred",imgBlurred)#me
+    imgThresh = cv2.adaptiveThreshold(imgBlurred, 255.0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,ADAPTIVE_THRESH_BLOCK_SIZE, ADAPTIVE_THRESH_WEIGHT)
+#    print("IN Preprocess")#me
+#    print("Printing ImgThresh",imgThresh)#me
+    #cv2.save("TestingGray.jpg",imgGrayscale)#me
+    #cv2.imshow("TestingThresh",imgThresh)#me
+    
+#        sys.exit(1)
+#    print("\n\n\n")
+    #i="2"
+    return imgGrayscale, imgThresh
+# end function
+
+###################################################################################################
+def extractValue(imgOriginal):
+    height, width, numChannels = imgOriginal.shape
+
+    imgHSV = np.zeros((height, width, 3), np.uint8)
+
+    imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+
+    imgHue, imgSaturation, imgValue = cv2.split(imgHSV)
+
+    return imgValue
+# end function
+
+###################################################################################################
+def maximizeContrast(imgGrayscale):
+
+    height, width = imgGrayscale.shape
+
+    imgTopHat = np.zeros((height, width, 1), np.uint8)
+    imgBlackHat = np.zeros((height, width, 1), np.uint8)
+
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    imgTopHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_BLACKHAT, structuringElement)
+
+    imgGrayscalePlusTopHat = cv2.add(imgGrayscale, imgTopHat)
+    imgGrayscalePlusTopHatMinusBlackHat = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+
+    return imgGrayscalePlusTopHatMinusBlackHat
+# end function
+
+
+
+
+
+
+
+
+
+
